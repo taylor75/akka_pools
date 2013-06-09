c05fcc80a36292c48b3e49c42e9f9784131b86f5
Chris Taylor <taylor75@gmail.com>
2013-06-09 15:37:14 -0500

    Now shutdown works through the jmx console, and updating the readme

--- README.md
+++ README.md
@@ -3,9 +3,14 @@ akka_pools
 
 Exploring akka remote modules with a focus on simplifying the job of achieving pools of distributed akka workers
 
-## Getting Started
-1. Build and package the dependencies by executing the ```./dist.sh``` command then ```cd dist```.
-2. Start the master task dispatching actor with ```./cluster-admin CalculatorTaskScheduler -s s01 -sp 8001 -sh localhost```
-3. Start a remote pool of (sample app) CalculatorWorkers ``` ./cluster-admin RemoteCalculatorPoolApp -l L01 -wp 8006 -s s01 -sp 8001 -ah localhost -w 3 -sh localhost```.  Note that many of these could be started on multiple remote hosts, just be sure they point to the scheduler correctly, and that they are named uniquely.
-4. Shutdown the remote system gracefully with ```./cluster-admin ShutdownSystem -ah localhost -sn RemoteCalculatorPoolApp -an L01 -ap 8006 -ac workercluster```
-5. Shutdown the CalculatorTaskScheduler actor system ```./cluster-admin ShutdownSystem -ah localhost -sn CalculatorTaskScheduler -an s01 -ap 8001 -ac workercluster```
+## Getting Started ... now featuring akka-cluster-experimental!
+1. Start the calculator sample app scheduler
+  1.1 Start an sbt window for the sample calculator app task scheduler with ```sbt -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false```
+  1.2 Start the scheduler ```run-main sample.remote.calculator.CalculatorTaskScheduler -sp 2551```
+2. Start the other cluster seed node (a remote calculator app worker pool)
+  2.1 Start an sbt window for the sample calculator app task scheduler with ```sbt -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false```
+  2.2 Start the remote node containing calculator actors ```run-main sample.remote.calculator.RemoteCalculatorPoolApp -l RemoteCalculatorPoolApp -sp 2552```
+3. Shutdown now can be managed via the jmx jconsole gui app.
+  3.1 In a separate window, simply type ```jconsole``` and double click on one of the jvms from sbt.
+
+* Note: It is now possible to start a RemoteWorkerPool for the calculator app on any accessible server and have it join the cluster and start doing calculations.  Just change the remote.netty.host in application.conf appropriately.  It is also no longer necessary to specify a port. ```run-main sample.remote.calculator.RemoteCalculatorPoolApp -l RemoteCalculatorPoolApp``` will just work.
--- src/main/scala/dlb/wpool/RemoteWorkerPool.scala
+++ src/main/scala/dlb/wpool/RemoteWorkerPool.scala
@@ -7,50 +7,56 @@ import dlb.scheduler.tasks._
 import com.typesafe.config.ConfigFactory
 import reflect.ClassTag
 import akka.cluster.{Cluster, Member, MemberStatus}
-import akka.cluster.ClusterEvent.MemberUp
-import akka.cluster.ClusterEvent.CurrentClusterState
-import dlb.scheduler.tasks.Expire
+import akka.cluster.ClusterEvent._
 import language.postfixOps
 import scala.concurrent.duration._
 
 class RemoteWorkerPool[W <: Actor : ClassTag](schedService:String, maxWorkers:Int) extends Actor with ActorLogging {
   val cfg = ConfigFactory.load().getConfig("workercluster")
   val sysName = cfg.getString("system-name")
-
-  log.info("Address Info: " +
-    List("akka", sysName, cfg.getString("scheduler-service.host"), cfg.getString("scheduler-service.port").toInt).mkString(", ")
-  )
-
   val cfgSchedulerAddress = Address("akka", sysName, cfg.getString("scheduler-service.host"), cfg.getString("scheduler-service.port").toInt)
-  println("cfgSchedulerAddress = " + cfgSchedulerAddress)
   val workers = context.actorOf(Props[W].withRouter(SmallestMailboxRouter(maxWorkers)), name = self.path.name+"_workers")
 
   val cluster = Cluster(context.system)
   var stopRequested = false
-
-  // subscribe to cluster changes, MemberUp
-  // re-subscribe when restart
-  override def preStart(){ cluster.subscribe(self, classOf[MemberUp]) }
-  override def postStop(){ cluster.unsubscribe(self) }
-
   import context.dispatcher
   val tickTask = context.system.scheduler.schedule(2 seconds, 2 seconds, self, "tick")
+  var taskCounter = 0
+
+  // subscribe to cluster changes, MemberUp, re-subscribe when restart
+  override def preStart(){
+    cluster.subscribe(self, classOf[MemberUp])
+    cluster.subscribe(self, classOf[MemberExited])
+  }
+
+  override def postStop(){
+    cluster.unsubscribe(self)
+    cluster.system.shutdown()
+  }
 
   def receive = {
+    case "tick" => if(!stopRequested) requestWork()
 
-    case task:Task ⇒ workers ! task
+    case task:Task =>
+      taskCounter+=1
+      workers ! task
 
-    case "tick" => requestWork()
-
-    case result: TaskResult ⇒
-      log.info(sender + "\t" + result)
+    case result: TaskResult => log.info(sender + "\t" + result)
+      taskCounter-=1
+      if(stopRequested && taskCounter==0) {
+        context.stop(self)
+      }
 
     case state: CurrentClusterState ⇒
       state.members.filter(_.status == MemberStatus.Up) foreach register
 
-    case Expire =>
-      log.warning("RemoteWorkerPool set to expire -- No new tasks will be requested and existing tasks will finish.")
-      stopRequested = true
+    case MemberExited(me) =>
+      log.warning("my address is " + cluster.selfAddress + " MemberExited address is " + me.address)
+      if (cluster.selfAddress == me.address) {
+        log.warning("RemoteWorkerPool MemberDowned -- No new tasks will be requested and existing tasks will finish.")
+        stopRequested = true
+        tickTask.cancel()
+      }
 
     case MemberUp(m) ⇒ register(m)
   }
@@ -64,7 +70,6 @@ class RemoteWorkerPool[W <: Actor : ClassTag](schedService:String, maxWorkers:In
   def requestWork(){
     if(!stopRequested) {
       val schedAddress = context.actorFor(RootActorPath(cfgSchedulerAddress) / "user" / schedService)
-      log.info("taskSchedulerAddress = " + schedAddress.path)
       schedAddress ! NeedWork
     }
   }
